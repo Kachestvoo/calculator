17d9ea442d578fcdbf226095e1b638b031c860de
Kachestvoo <pingvin41k@hotmail.com>
2023-10-21 19:04:04 +0300

    fixes logic

--- calc/main.c
+++ calc/main.c
@@ -3,40 +3,28 @@
 #include <math.h>
 
 int main(){
-    int i = 0;
+    int i = 0, k = 0;
     float a = 0, b = 0;
-    char op;
-    printf ("example \n"); // Выводит начальный экран калькулятора
-
-    while (i>=0){ //Если i<0 программа прекращает работу
-        printf("Input i ");
-        scanf ("%d", &i);
-        if (i == 0) // Если i = 0, выводит начальный экран калькулятора
-            printf ("example \n");
-
-        if (i == 1){ // Arithmetic
-            printf ("+ - / * %");
-            scanf ("%f %c %f", &a, &op, &b);
-            if (op == '+')
-                printf ("%.2f + %.2f = %.2f \n", a, b, a+b);
-            if (op == '-')
-                printf ("%.2f + %.2f = %.2f \n", a, b, a-b);
-            if (op == '/')
-                printf ("%.2f / %.2f = %f \n", a, b, a/b);
-            if (op == '%'){
-                int c = 0, d = 0;
-                c = a;
-                d = b;
-                printf ("%f / %f = %d \n", a, b, c%d);
+    while (i>=0){
+        if (i == 0){
+            printf ("example \n"); // main interface
+            scanf ("%d", &i);
+            if (i == 0)
+                exit (0);
+            if (i == 1){ // math
+                printf ("example math \n");
+                scanf ("%d", &k);
+                if (k == 0)
+                    i = 0;
+                if (k == 1){
+                    printf("Input a: ");
+                    scanf ("%f", &a);
+                    printf("Input b: ");
+                    scanf ("%f", &b);
+                    printf ("result %.2f \n", a+b);
+                }
             }
-            if (op == '^')
-                printf ("%.2f ^ %.2f = %.2f \n", a, b, pow(a,b));
-            if (op == 's') // вычисление корня
-                printf ("(%.2f) sqrt(%.2f) = %.2f \n", b, a, pow(a,1/b));
-        }
-
-        if (i == 2){ // Trigonometry
-
         }
     }
+
 }
